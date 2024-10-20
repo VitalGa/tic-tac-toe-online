a0785bb5e32a98ff281b9e26282e0d16f4cc9342
VitalGa <vitalga10@gmail.com>
2024-10-20 11:31:04 +0300

    add eslint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3315577513ef16d8"; }
+/******/ 		__webpack_require__.h = function() { return "4f0e0e675368798c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
